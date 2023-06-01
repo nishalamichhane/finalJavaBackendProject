460a98d4588e26b6f6ab36f88ff260c37c9e49cc
nishalamichhane <nishalamichhane9@gmail.com>
2023-06-01 11:26:07 +0200

    Springboot setup has been done!

---
+++ src/main/java/com/finalProject/nisha/NishaApplication.java
@@ -0,0 +1,13 @@
+package com.finalProject.nisha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NishaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NishaApplication.class, args);
+	}
+
+}
