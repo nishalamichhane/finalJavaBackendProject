b296a22acecc200d7b59a986491910665ef9ba62
nishalamichhane <nishalamichhane9@gmail.com>
2023-06-14 11:13:13 +0200

    Created Product models and Dto

---
+++ src/main/java/com/finalProject/nisha/dtos/ProductDto.java
@@ -0,0 +1,8 @@
+package com.finalProject.nisha.dtos;
+
+public class ProductDto {
+    private long id;
+    private String productName;
+    private double unitPrice;
+    private long categoryId;
+}
---
+++ src/main/java/com/finalProject/nisha/models/product.java
@@ -0,0 +1,9 @@
+package com.finalProject.nisha.models;
+
+public class product {
+    private long id;
+    private String productName;
+    private double unitPrice;
+    private long categoryId;
+
+}
