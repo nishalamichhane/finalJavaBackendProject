0680fae5803cd54413623e400b0325688e917ff1
nishalamichhane <nishalamichhane9@gmail.com>
2023-06-14 14:47:18 +0200

    OrderlineDto, Orderline model, OrderlineRepository has been added

---
+++ src/main/java/com/finalProject/nisha/dtos/OrderlineDto.java
@@ -0,0 +1,10 @@
+package com.finalProject.nisha.dtos;
+
+public class OrderlineDto {
+    public long id;
+    public long productId;
+    public String productName;
+    public short quantity;
+    public double unitPrice;
+    public double subTotal;
+}
