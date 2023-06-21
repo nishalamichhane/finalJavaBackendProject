fdf256e77675d48a13078c705008000daf6a0e79
nishalamichhane <nishalamichhane9@gmail.com>
2023-06-21 13:08:27 +0200

    Invoice Service has been added

--- src/main/java/com/finalProject/nisha/models/Invoice.java
+++ src/main/java/com/finalProject/nisha/models/Invoice.java
@@ -1,8 +1,18 @@
 package com.finalProject.nisha.models;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Date;
+@Getter
+@Setter
+@Entity
+@Table(name = "invoice")
 
 public class Invoice {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private double totalAmount;
     private Date invoiceDate;
