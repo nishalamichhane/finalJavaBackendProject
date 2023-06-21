545f66150ebcc7e64194819aa0d25023da457ca1
nishalamichhane <nishalamichhane9@gmail.com>
2023-06-21 12:17:32 +0200

    Invoice models has been created

---
+++ src/main/java/com/finalProject/nisha/models/Invoice.java
@@ -0,0 +1,11 @@
+package com.finalProject.nisha.models;
+
+import java.util.Date;
+
+public class Invoice {
+    private long id;
+    private double totalAmount;
+    private Date invoiceDate;
+    private long userId;
+    private long orderId;
+}
