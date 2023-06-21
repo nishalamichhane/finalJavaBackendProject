f1397dfffaec0cad6adb31c7c9e929567072b497
nishalamichhane <nishalamichhane9@gmail.com>
2023-06-21 12:20:19 +0200

    InvoiceDto has been created

---
+++ src/main/java/com/finalProject/nisha/dtos/InvoiceDto.java
@@ -0,0 +1,11 @@
+package com.finalProject.nisha.dtos;
+
+import java.util.Date;
+
+public class InvoiceDto {
+    public long id;
+    public double totalAmount;
+    public Date invoiceDate;
+    public long userId;
+    public long orderId;
+}
