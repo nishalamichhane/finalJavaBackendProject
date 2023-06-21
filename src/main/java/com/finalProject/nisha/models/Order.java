163acca85b7b49cae25ee7d183eeb4e96046dd65
nishalamichhane <nishalamichhane9@gmail.com>
2023-06-21 21:35:27 +0200

    one to many in order has been added

--- src/main/java/com/finalProject/nisha/models/Order.java
+++ src/main/java/com/finalProject/nisha/models/Order.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 @Entity
@@ -15,4 +17,6 @@ public class Order {
     private long orderlineId;
     private double total;
     private long userId;
+    @OneToMany(mappedBy = "Order")
+    private List<Orderline> orderlines;
 }
