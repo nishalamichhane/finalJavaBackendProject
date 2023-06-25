53cd24c9dc225d3492a5dbad8713cf8a3c4d5fab
nishalamichhane <nishalamichhane9@gmail.com>
2023-06-25 05:05:35 +0200

    Added User, Authority and the AuthorityKey as models

---
+++ src/main/java/com/finalProject/nisha/models/AuthorityKey.java
@@ -0,0 +1,8 @@
+package com.finalProject.nisha.models;
+
+import java.io.Serializable;
+
+public class AuthorityKey implements Serializable {
+    private String username;
+    private String authority;
+}
