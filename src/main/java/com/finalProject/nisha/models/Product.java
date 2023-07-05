713b3ebf97ed7ca038eed8c1440a2a7e27c44b8a
nishalamichhane <nishalamichhane9@gmail.com>
2023-07-05 11:57:15 +0200

    Validation for products has been added to the system.

--- src/main/java/com/finalProject/nisha/models/Product.java
+++ src/main/java/com/finalProject/nisha/models/Product.java
@@ -1,5 +1,6 @@
 package com.finalProject.nisha.models;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,6 +14,7 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
     private String productName;
     private double unitPrice;
     private String description;
